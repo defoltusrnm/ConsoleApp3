235eab5b6573647baac89c9ecbedcf016453c78c
srt_ansmv <siarhei.anisimau@innowise-group.com>
2021-12-16 10:27:29 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        
+        static async Task Main(string[] args)
+        {
+            string folderPath = Directory.GetCurrentDirectory();
+
+            string[] files = Directory.GetFiles(folderPath, "*.txt", SearchOption.TopDirectoryOnly);
+
+            string pattern = "";
+
+            RegexFileSpliter spliter = new RegexFileSpliter(pattern);
+
+            foreach (string file in files)
+            {
+                await spliter.SplitAsync(file, x => string.Join(" ", x));
+            }
+        }
+    }
+}
---
+++ ConsoleApp3/RegexFileSpliter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class RegexFileSpliter
+    {
+        private readonly Regex _regex;
+
+        public RegexFileSpliter(string pattern)
+        {
+            _regex = new Regex(pattern);
+        }
+        
+        public async Task SplitAsync(string path, Func<string[], string> joinFunc)
+        {
+            using StreamReader reader = File.OpenText(path);
+            using StreamWriter writer = File.CreateText($"{Path.GetFileNameWithoutExtension(path)}Parsed.txt");
+
+            do
+            {
+                string line = await reader.ReadLineAsync();
+
+                await writer.WriteLineAsync(joinFunc(_regex.Split(line)));
+            }
+            while (!reader.EndOfStream);
+        }
+
+    }
+}
